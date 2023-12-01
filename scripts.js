829e70a8fb45c8cb8ad5e52d125cc1932f79903c
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-02 02:14:07 +0600

    try different approach

--- scripts.js
+++ scripts.js
@@ -6,11 +6,21 @@ const eyeIcon = document.querySelector(".js-eye-icon");
 showHideIcon.addEventListener("mousedown",
   () => {
     password.setAttribute("type", "text");
-    eyeIcon.src = "./images/eye.png"
-  })
+    eyeIcon.src = "./images/eye.png";
+  }) //this will work onmousedown
 
 showHideIcon.addEventListener("mouseup",
   () => {
     password.setAttribute("type", "password");
     eyeIcon.src = "./images/hidden.png"
-  })
+  }) //this will work onmouseup
+
+// showHideIcon.onclick = () => {
+//   if (password.type === "password") {
+//     password.setAttribute("type", "text");
+//     eyeIcon.src = "./images/eye.png";
+//   } else {
+//     password.setAttribute("type", "password");
+//     eyeIcon.src = "./images/hidden.png";
+//   }
+// } //this will work onclick
